04356f7db6897d231d819c2c99fa7ed91aadb2ff
learnforpractice <learnforpractice@gmail.com>
2019-07-02 17:55:44 +0800

    Add print

--- language/stdlib/natives/src/db.rs
+++ language/stdlib/natives/src/db.rs
@@ -7,9 +7,42 @@ use std::borrow::Borrow;
 use tiny_keccak::Keccak;
 use types::byte_array::ByteArray;
 
+extern crate libc;
+use libc::size_t;
+
+#[link(name = "eosiolib_native")]
+extern {
+    fn say_hello();
+    fn read_action_data(msg: *mut u8, len: size_t) -> i32;
+    fn action_data_size() -> i32;
+    fn checktime();
+    fn prints_l( msg: *const u8, len: size_t);
+}
+
+pub fn vm_checktime() {
+    unsafe {
+        checktime();
+    }
+}
+
+pub fn vm_print(msg: &[u8]) {
+    unsafe {
+        prints_l(msg.as_ptr(), msg.len());
+    }
+}
+
 pub fn native_print<T: StackAccessor>(mut accessor: T) -> Result<CostedReturnType> {
-    let hash_arg = accessor.get_byte_array()?;
-    println!("{}", hash_arg);
+    let hash_arg = match accessor.get_byte_array() {
+        Ok(hash_arg)  => hash_arg,
+        Err(e) => return Err(e),
+    };
+//    let hash_arg = accessor.get_byte_array().unwrap().as_bytes();
+    /*
+    hash_arg.as_ptr();
+    hash_arg.len();
+    */
+    vm_print(hash_arg.as_bytes());
+//    println!("{}", hash_arg);
     Ok(CostedReturnType::new(0, NativeReturnType::Bool(true)))
 }
 
