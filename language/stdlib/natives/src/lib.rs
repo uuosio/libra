d72da8e43da6f30d63d1fe7a821c3a24d22174f4
learnforpractice <learnforpractice@gmail.com>
2019-07-06 17:24:12 +0800

    Implement get_bool in StackAccessor for StackAccessorMocker in cost_synthesis/src/natives.rs

--- language/stdlib/natives/src/lib.rs
+++ language/stdlib/natives/src/lib.rs
@@ -7,6 +7,7 @@ pub mod primitive_helpers;
 pub mod signature;
 pub mod db;
 pub mod debug;
+pub mod vm_api;
 
 extern crate libc;
 use libc::size_t;
@@ -19,6 +20,17 @@ extern {
     fn checktime();
     fn is_account( name: u64 ) -> bool;
     fn current_receiver() -> u64;
+
+    fn db_store_i64(scope: u64, table: u64, payer: u64, id: u64,  data: *const u8, len: size_t);
+    fn db_update_i64(iterator: i32, payer: u64, data: *const u8, len: size_t);
+    fn db_remove_i64(iterator: i32);
+    fn db_get_i64(iterator: i32, data: *mut u8, len: size_t) -> i32;
+    fn db_next_i64(iterator: i32, primary: *mut u64) -> i32;
+    fn db_previous_i64(iterator: i32, primary: *mut u64) -> i32;
+    fn db_find_i64(code: u64, scope: u64, table: u64, id: u64) -> i32;
+    fn db_lowerbound_i64(code: u64, scope: u64, table: u64, id: u64) -> i32;
+    fn db_upperbound_i64(code: u64, scope: u64, table: u64, id: u64) -> i32;
+    fn db_end_i64(code: u64, scope: u64, table: u64) -> i32;
 }
 
 pub fn vm_checktime() {
@@ -33,8 +45,74 @@ pub fn vm_current_receiver() ->u64 {
     }
 }
 
+pub fn vm_is_account(name: u64) ->bool {
+    unsafe {
+        is_account(name)
+    }
+}
+
+pub fn vm_db_store_i64(scope: u64, table: u64, payer: u64, id: u64,  data: &[u8]) {
+    return unsafe {
+        db_store_i64(scope, table, payer, id,  data.as_ptr(), data.len())
+    }
+}
+
+pub fn vm_db_update_i64(iterator: i32, payer: u64, data: &[u8]) {
+    unsafe {
+        db_update_i64(iterator, payer, data.as_ptr(), data.len());
+    }
+}
+
+pub fn vm_db_remove_i64(iterator: i32) {
+    unsafe {
+        db_remove_i64(iterator);
+    }
+}
+
+pub fn vm_db_get_i64(iterator: i32, data: &mut [u8]) {
+    unsafe {
+        db_get_i64(iterator, data.as_mut_ptr(), data.len());
+    }
+}
+
+pub fn vm_db_next_i64(iterator: i32, primary: &mut u64) -> i32 {
+    unsafe {
+        db_next_i64(iterator, primary as *mut u64)
+    }
+}
+
+pub fn vm_db_previous_i64(iterator: i32, primary: &mut u64) -> i32 {
+    unsafe {
+        db_previous_i64(iterator, primary as *mut u64)
+    }
+}
+
+pub fn vm_db_find_i64(code: u64, scope: u64, table: u64, id: u64) -> i32 {
+    unsafe {
+        db_find_i64(code, scope, table, id)
+    }
+}
+
+pub fn vm_db_lowerbound_i64(code: u64, scope: u64, table: u64, id: u64) -> i32 {
+    unsafe {
+        db_lowerbound_i64(code, scope, table, id)
+    }
+}
+
+pub fn vm_db_upperbound_i64(code: u64, scope: u64, table: u64, id: u64) -> i32 {
+    unsafe {
+        db_upperbound_i64(code, scope, table, id)
+    }
+}
+
+pub fn vm_db_end_i64(code: u64, scope: u64, table: u64) -> i32 {
+    unsafe {
+        db_end_i64(code, scope, table)
+    }
+}
+
 /*
-uint32_t read_action_data( void* msg, uint32_t len )
+uint32_t read_action_data( void* msg, len: size_t )
 uint32_t action_data_size()
 void require_recipient( account_name name )
 void require_auth( account_name name )
